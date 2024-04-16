779aaa629e19114f805609e5e2c9725e3fa97fbb
Bensikrac <94077504+Bensikrac@users.noreply.github.com>
2024-04-16 20:22:27 +0200

    Replace VK_ICD_FILENAMES with VK_DRIVER_FILES

--- scripts/attachgpu.sh
+++ scripts/attachgpu.sh
@@ -7,5 +7,5 @@ echo -n "0000:01:00.1" > /sys/bus/pci/drivers/snd_hda_intel/bind
 #setup currentenv for gpu loader switching
 echo -n "__GLX_VENDOR_LIBRARY_NAME=nvidia
 __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
-VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json" > $HOME/configs/scripts/gpuscript/currentenv
+VK_DRIVER_FILES=/usr/share/vulkan/icd.d/nvidia_icd.json" > $HOME/configs/scripts/gpuscript/currentenv
 #replace above with your preffered location
