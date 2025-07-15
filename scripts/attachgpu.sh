c8affb150f909a2a38fb516901478303398defd8
ProgrammedInsanity <ProgrammedInsanity@proton.me>
2025-07-15 20:35:38 +0200

    fix: remove nvidia module parameters that do nothing in attachgpu.sh
    If you want the behavior of loading all those modules you would use `modprobe -a nvidia nvidia_modeset nvidia_uvm`

--- scripts/attachgpu.sh
+++ scripts/attachgpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rmmod vfio_pci vfio_pci_core vfio_iommu_type1 vfio
 modprobe nvidia_drm modeset=1 fbdev=0
-modprobe nvidia nvidia_modeset nvidia_uvm 
+modprobe nvidia
 echo -n "0000:01:00.1" > /sys/bus/pci/drivers/snd_hda_intel/bind
 
 #setup currentenv for gpu loader switching
