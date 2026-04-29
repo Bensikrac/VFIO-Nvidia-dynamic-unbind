91cf24143087225809a4878f836f88589888e802
Bensikrac <94077504+Bensikrac@users.noreply.github.com>
2026-04-29 07:27:07 +0200

    Update gpuloader.shsh Rename to VK_DRIVER_FILES

--- scripts/gpuloader.sh
+++ scripts/gpuloader.sh
@@ -3,7 +3,7 @@ command=$1
 
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
-export VK_ICD_FILES=/usr/share/vulkan/icd.d/nvidia_icd.json
+export VK_DRIVER_FILES=/usr/share/vulkan/icd.d/nvidia_icd.json
 export __NV_PRIME_RENDER_OFFLOAD=1
 export __VK_LAYER_NV_optimus=NVIDIA_only
 
